80a13ddf7b7625d1ed8de42fbee0bf35371370f5
flofeyka <danil.bashirov.08@gmail.com>
2024-07-29 01:39:03 +0500

    reverse animation for menu. mobile adaptation

--- src/components/leftMenu/CountriesList.tsx
+++ src/components/leftMenu/CountriesList.tsx
@@ -1,26 +1,35 @@
 'use client'
 
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function CountriesList({ continent }: { continent: any }) {
     const [activeCountryIndex, setActiveCountryIndex] = useState<string>("");
 
     return <div className='countryItem'>
-        {continent.children.map((children: any) => <motion.div initial={{ opacity: 0, y: -10 }} // Начальное состояние
-            animate={{ opacity: 1, x: 0, y: 0 }}    // Конечное состояние
-            transition={{ duration: 0.25 }} key={children.key}>
-            <div onClick={() => setActiveCountryIndex(activeCountryIndex === children.key ? "" : children.key)}
-                id={activeCountryIndex == children.key ? 'menu-item' + "-active" : "menu-item"}>
-                {children.label} <span style={{ marginLeft: "12px", display: "flex" }}> {activeCountryIndex === children.key ? <span>-</span> : <span>+</span>} </span>
-            </div>
-            <div>
-                {activeCountryIndex == children.key && <div style={{ marginLeft: "15px" }}>
-                    {children.children.map((child: any) => <motion.div initial={{ opacity: 0, y: -10 }} // Начальное состояние
-                        animate={{ opacity: 1, x: 0, y: 0 }}    // Конечное состояние
-                        transition={{ duration: 0.25 }} id="menu-item" key={child.key}>{child.label}</motion.div>)}
-                </div>}
-            </div>
-        </motion.div>)}
+        <AnimatePresence>
+            {continent.children && (continent.children.map((children: any) => <motion.div
+                initial={{ opacity: 0, height: 0 }} exit={{ opacity: 0, height: 0 }} // Начальное состояние
+                animate={{ opacity: 1, height: 'auto' }}
+                className="country-item" key={children.key}>
+                <div style={{ fontSize: "18px" }} onClick={() => setActiveCountryIndex(activeCountryIndex === children.key ? "" : children.key)}
+                    id={activeCountryIndex == children.key ? 'menu-item' + "-active" : "menu-item"}>
+                    {children.label} <span style={{ marginLeft: "12px", display: "flex" }}> {activeCountryIndex === children.key ? <span>-</span> : <span>+</span>} </span>
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <AnimatePresence>
+                        {activeCountryIndex == children.key && <div style={{ marginLeft: "15px" }}>
+                            {children.children.map((child: any) => <motion.div style={{ fontSize: "14px", marginBottom: "5px" }}
+                                initial={{ opacity: 0, height: 0 }} exit={{ opacity: 0, height: 0 }} // Начальное состояние
+                                animate={{ opacity: 1, height: 'auto' }}
+                                id="menu-item" key={child.key}>{child.label}</motion.div>
+                            )}
+                        </div>}
+                    </AnimatePresence>
+
+                </div>
+            </motion.div>
+            ))}
+        </AnimatePresence>
     </div>
 }
